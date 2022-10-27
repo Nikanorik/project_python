ab8f884d948599687436f860c365ce631ddda21c
Nikanorik <nikanorov.company@gmail.com>
2022-10-27 17:56:53 +0300

    Add new help file dictionary

--- Part_2/english_dictionary.py
+++ Part_2/english_dictionary.py
@@ -11,7 +11,19 @@ def main():
                   'is he from USA': 'Он из Америки?', 'how old are you': 'сколько тебе лет?', 'ceiling': 'потолок',
                   'next time': 'следующий раз', 'take an english exam': ' сдавать английский экзамен',
                   'do makeovers': 'делать ремонт', 'feed the cat': 'кормить кота',
-                  'enter the university': 'поступать в университет'}
+                  'enter the university': 'поступать в университет', 'stay at home': 'оставаться дома',
+                  'how will you get visa': 'как ты будешь получать визу?',
+                  'take english classes': 'брать уроки английского языка', 'tell the truth': 'говорить правду',
+                  'invite': 'пригласить', 'organize a party': 'устраивать вечеринку',
+                  'repair the computer': 'чинить компьютер', 'solve the problem': 'решить проблему',
+                  'take an exam': 'сдавать экзамен', 'truth': 'правда', 'the day after tomorrow': 'послезавтра',
+                  'how many classes will you have tomorrow': 'как много уроков ты имеешь завтра?',
+                  'i study well at school': 'я учусь хорошо в школе', 'let is talk': 'позволь нам поговорить',
+                  'make a present': 'делать подарки', 'be married': 'быть замужем',
+                  'groccery store': 'продуктовый магазин', 'give present': 'дарить подарки',
+                  'brush teeth': 'чистить зубы', 'take a shower': 'принимать душ', 'film videos': 'делать видео',
+                  'run in the forest': 'бегать в лесу', 'why are you at home': 'почему ты дома?', 'dirty': 'грязный',
+                  'single': 'одинокий', 'where are you from': 'откуда ты?', 'there': 'там', 'i am out': 'я на руже'}
     flag = True
     check_plus = 0
     check_minus = 0
